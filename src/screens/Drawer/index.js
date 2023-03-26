0408569ca9153bb893f27c86f75429f6cb23c516
Harsh Singhal <Harshsinghal24dec@gmail.com>
2023-03-26 23:42:20 +0530

    .

--- src/screens/Drawer/index.js
+++ src/screens/Drawer/index.js
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {black, blue, white} from '../../constants/colors';
 
 const Drawer = () => {
   return (
-    <View>
-      <Text>Drawer</Text>
+    <View style={styles.containerMain}>
+      <View style={styles.drawerContainer}>
+        <Text style={{color: black}}>Drawer</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Drawer
+const styles = StyleSheet.create({
+  containerMain: {
+    flex: 1,
+    backgroundColor: blue,
+  },
+  drawerContainer: {
+    backgroundColor: '#FBFBFD',
+    width: '76%',
+    flex: 1
+  },
+});
+
+export default Drawer;
